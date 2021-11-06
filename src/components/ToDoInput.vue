0d813be60e7add4a960c3a83f5cf5ae4ba24a018
CX개발팀 <yuio1126@gmail.com>
2021-11-06 16:41:20 +0900

    feat: period 추가

--- src/components/ToDoInput.vue
+++ src/components/ToDoInput.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="todoinput">
-    <input type="text" v-model="content" @keyup.enter="addToDo" />
+    <input type="text" v-model="content" @keypress.enter="addToDo" />
+    <div id="v-model-select" class="demo">
+      <select v-model="period">
+        <option disabled value="">Please select one</option>
+        <option value="dawn">새벽</option>
+        <option value="morning">아침</option>
+        <option value="afternoon">점심</option>
+        <option value="night">저녁</option>
+      </select>
+    </div>
     <button type="button" @click="addToDo">제출</button>
   </div>
 </template>
@@ -9,13 +18,17 @@
 export default {
   data() {
     return {
-      content: ''
+      content: '',
+      period: ''
     }
   },
   methods: {
     addToDo() {
       // vuex commit
-      this.$store.commit('addTodo', this.content)
+      var payload = {}
+      payload.content = this.content
+      payload.period = this.period
+      this.$store.commit('addTodo', payload)
       this.content = ''
     }
   }
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="todolist">
     <div id="container" v-for="data in todos" :key="data.id">
-      <li id="text">{{ data }}</li>
+      <div id="item">
+        <div id="text">{{ data.content }}</div>
+        <div id="period">{{ this.periods[data.period] }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {periods: {dawn: '새벽', morning: '아침', afternoon: '낮', night: '저녁'}}
+  },
   computed: {
     todos() {
       return this.$store.state.todos
     }
-  }
+  },
+  methods: {}
 }
 </script>
 
-<style></style>
+<style>
+#container {
+  width: 400px;
+  height: 20px;
+  border-radius: 10px;
+}
+#container #item {
+}
+</style>
