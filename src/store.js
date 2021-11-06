fa8187b0a440c2fe9231a5a0a49f000d333554fc
CX개발팀 <yuio1126@gmail.com>
2021-11-06 15:36:59 +0900

    fix: remove useless

--- src/store.js
+++ src/store.js
@@ -4,16 +4,12 @@ const store = createStore({
   state() {
     return {
       // 컴포넌트에서 접근가능
-      count: 0,
       todos: []
     }
   },
   mutations: {
     // state 변수는 외부 컴포넌트에서 수정 불가능하므로
     // mutation 내 정의된 함수를 이용해야 상태의 변경 가능하다
-    implement(state) {
-      state.count = state.count + 1
-    },
     addTodo(state, content) {
       state.todos.push(content)
     }
